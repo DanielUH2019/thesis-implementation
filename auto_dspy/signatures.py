447d0f0d656e45586898e4987e9b5b08ae3ced03
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-11-13 02:20:09 -0500

    Impl basic signatures for experimentation

---
+++ auto_dspy/signatures.py
@@ -0,0 +1,80 @@
+from .algorithms_base import AlgorithmSignature
+from dspy.signatures import InputField, OutputField
+
+
+class GenerateSearchQuery(AlgorithmSignature):
+    """Write a simple search query that will help answer a complex question."""
+
+    context = InputField(desc="may contain relevant facts")
+    question = InputField()
+    query = OutputField()
+
+
+class GenerateAnswer(AlgorithmSignature):
+    """Answer questions with short factoid answers."""
+
+    context = InputField(desc="may contain relevant facts")
+    question = InputField()
+    answer = OutputField(desc="often between 1 and 5 words")
+
+
+class BasicQA(AlgorithmSignature):
+    """Answer questions with short factoid answers."""
+
+    question = InputField()
+    answer = OutputField(desc="often between 1 and 5 words")
+
+
+class SummarizeText(AlgorithmSignature):
+    """Generate a concise summary of a given text."""
+
+    text = InputField(desc="the input text to be summarized")
+    summary = OutputField(desc="a concise summary of the input text")
+
+
+class ImageCaptioning(AlgorithmSignature):
+    """Generate a descriptive caption for an input image."""
+
+    image = InputField(desc="the input image to be captioned")
+    caption = OutputField(desc="a descriptive caption for the input image")
+
+
+class LanguageTranslation(AlgorithmSignature):
+    """Translate text from one language to another."""
+
+    source_text = InputField(desc="the text to be translated")
+    target_language = InputField(desc="the language to translate to")
+    translated_text = OutputField(desc="the translated text")
+
+
+class ZeroShotTextClassification(AlgorithmSignature):
+    """Analyze a given piece of text and classify into some candidate labels"""
+
+    text = InputField(desc="the text for sentiment analysis")
+    candidate_labels = InputField(desc="labels to choose for the text classification")
+    predicted_classification = OutputField(
+        desc="the classification of the input text, e.g., positive, negative, neutral"
+    )
+
+
+class TextClassification(AlgorithmSignature):
+    """Analyze a given piece of text and classify"""
+
+    text = InputField(desc="the text for sentiment analysis")
+    predicted_classification = OutputField(
+        desc="the classification of the input text, e.g., positive, negative, neutral"
+    )
+
+
+class ImageClassification(AlgorithmSignature):
+    """Assign a label to an entire image"""
+
+    image = InputField()
+    label = OutputField()
+
+
+class ZeroShotImageClassification(AlgorithmSignature):
+    """Classify an images into one of several classes, without any prior training or knowledge of the classes."""
+
+    image = InputField()
+    label = OutputField()
