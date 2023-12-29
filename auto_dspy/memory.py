d9a62886b9a730f66579afe0584eab2ea12844e2
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-12-28 21:13:54 -0500

    Change qdrant for chromadb in memory

--- auto_dspy/memory.py
+++ auto_dspy/memory.py
@@ -1,9 +1,12 @@
 from typing import Optional
-from qdrant_client import QdrantClient
-from qdrant_client.models import Filter, PointStruct, FieldCondition, PointIdsList
+
+
+
 from pydantic import BaseModel
 from typing import Union, Any
-
+from sentence_transformers import SentenceTransformer
+from uuid import uuid4
+import chromadb
 
 class MemoryDataModel(BaseModel):
     data: str
@@ -19,40 +22,45 @@ class ValueStoreObjectModel(BaseModel):
 
 class Memory:
     def __init__(self, collection_name: str = "memory") -> None:
-        self.collection_name = collection_name
-        self.client = self._create_vector_db()
+        self.client = chromadb.Client()
+        self.collection = self.client.create_collection(collection_name)
         # Given a key retrieved from the vector database, this dictionary will return his value
         # This value can be a number, a function, a list, etc
         self.value_store: dict[str, Any] = {}
         # Given a key, this dictionary will return the id of the vector in the vector database
         self._id_store: dict[str, Union[int, str]] = {}
+        self._all_ids = []
 
-    def _create_vector_db(self) -> QdrantClient:
-        client = QdrantClient(":memory:")
-        client.create_collection(
-            collection_name=self.collection_name,
-            vectors_config=client.get_fastembed_vector_params(),
+    
+    def get_all_data(self):
+        return self.collection.get()
+        
+    
+    def upload_records(self, ids, documents, metadatas):
+        self.collection.add(ids=ids, documents=documents, metadatas=metadatas)
+    
+    def query(self, query: str, query_filter: Optional[chromadb.Where] = None, limit: int = 1):
+        # print("mmmm", self.get_all_data())
+        # print('query: ', query)
+        results = self.collection.query(
+            query_texts=[query],
+            where=query_filter,
+            n_results=limit
         )
-        return client
-
-    def query(self, query: str, query_filter: Optional[Filter] = None, limit: int = 5):
-        return self.client.query(
-            collection_name="memory",
-            query_text=query,
-            query_filter=query_filter,
-        )
+        # print(f"query results {results}")
+        return results
 
     def retrieve_stored_value(self, key: str, description: str):
         if key in self.value_store:
             return self.value_store[key]
 
         search_result = self.query(query=description, limit=1)
-        if len(search_result) == 0:
+        if len(search_result['ids'][0]) == 0:
             raise ValueError(
                 f"Could not find a similar values for key {key} and description: {description}"
             )
 
-        most_similar_key = search_result[0].metadata["key_name"]
+        most_similar_key = search_result['metadatas'][0][0]["key_name"]
         if most_similar_key not in self.value_store:
             raise ValueError(
                 f"The most similar key {most_similar_key} is not in the value store"
@@ -61,29 +69,42 @@ class Memory:
         return self.value_store[most_similar_key]
 
     def insert_to_value_store(self, data: list[ValueStoreObjectModel]):
-        ids = self.client.add(
-            collection_name="value_store",
+        ids = [str(uuid4()) for i in data]
+        self.collection.add(
             documents=[x.description for x in data],
-            metadata=[{"key_name": x.key_name} for x in data],
+            metadatas=[{"key_name": x.key_name} for x in data],
+            ids=ids
         )
         for i in range(len(data)):
             self.value_store[data[i].key_name] = data[i].value
             self._id_store[data[i].key_name] = ids[i]
 
-    def insert(self, data: list[str]):
-        self.client.add(
-            collection_name=self.collection_name,
-            documents=[x for x in data],
+    def insert(self, documents, metadatas) -> list[str]:
+        if len(documents) == 0:
+            return []
+        
+        # print(f'documents to insert {documents}')
+        ids = [str(uuid4()) for d in documents]
+        self.collection.add(
+            documents=documents,
+            metadatas=metadatas,
+            ids=ids
         )
+        # print(f'all_data {self.get_all_data()}')
+        return ids
+    
+    def update(self, id: str, data: str):
+        self.collection.upsert(ids=[id], documents=[data])
+
 
     def delete_from_store(self, key: str):
         id_to_delete = self._id_store[key]
-        self.client.delete(
-            self.collection_name, points_selector=PointIdsList(points=[id_to_delete])
+        self.collection.delete(
+            ids=[id_to_delete]
         )
         del self.value_store[key]
 
-    def delete_from_db(self, id: str|int):
-        self.client.delete(
-            self.collection_name, points_selector=PointIdsList(points=[id])
+    def delete_from_db(self, id: str):
+        self.collection.delete(
+            ids=[id]
         )
