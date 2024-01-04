4d4a84155e0427fc5ac624226de783a2710c3973
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-01-03 21:44:07 -0500

    Remove  prints in memory

--- auto_dspy/memory.py
+++ auto_dspy/memory.py
@@ -40,12 +40,9 @@ class Memory:
     def query(
         self, query: str, query_filter: Optional[chromadb.Where] = None, limit: int = 1
     ):
-        # print("mmmm", self.get_all_data())
-        # print('query: ', query)
         results = self.collection.query(
             query_texts=[query], where=query_filter, n_results=limit
         )
-        # print(f"query results {results}")
         return results
 
     def retrieve_stored_value(self, description: str):
@@ -62,7 +59,7 @@ class Memory:
         most_similar_key = search_result["metadatas"][0][0]["key_name"]
         most_similar_id = search_result["ids"][0][0]
         if most_similar_id not in self.value_store:
-            print(f" value store {self.value_store}")
+            # print(f" value store {self.value_store}")
             raise ValueError(
                 f"The most similar key {most_similar_key} with description {search_result['documents'][0][0]} is not in the value store"
             )
@@ -71,13 +68,12 @@ class Memory:
 
     def insert_to_value_store(self, data: list[ValueStoreObjectModel]):
         ids = [str(uuid4()) for i in data]
-        print("data adding", [print(x.description for x in data)])
         self.collection.add(
             documents=[x.description for x in data],
             metadatas=[{"key_name": x.key_name} for i, x in enumerate(data)],
             ids=ids,
         )
-        print(f"mmmm", print(self.get_all_data()))
+        # print(f"mmmm", print(self.get_all_data()))
         for i in range(len(data)):
             self.value_store[ids[i]] = data[i].value
             self._id_store[data[i].key_name] = ids[i]
@@ -86,17 +82,14 @@ class Memory:
         if len(documents) == 0:
             return []
 
-        print(f'documents to insert {documents}')
         ids = [str(uuid4()) for d in documents]
         self.collection.add(documents=documents, metadatas=metadatas, ids=ids)
-        # print(f'all_data {self.get_all_data()}')
         return ids
 
     def update(self, id: str, data: str):
         self.collection.upsert(ids=[id], documents=[data])
 
     def delete_from_store(self, ids_to_delete: list[str]):
-        # id_to_delete = self._id_store[key]
         self.collection.delete(ids=ids_to_delete)
         for id in ids_to_delete:
             del self.value_store[id]
